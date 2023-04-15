34d54edc7922df1ea0a82c7353f24249a8cd45ae
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2023-04-15 10:50:19 +0200

    Create parametrizadoDSL.groovy

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,27 @@
+job('ejemplo-job-DSL-desde-GitHub') {
+  description('Job DSL de ejemplo para el curso de Jenkins')
+  scm {
+    git('https://github.com/agarciafer/laboratorio.git', 'master') { node ->
+      node / gitConfigName('agarciaf')
+      node / gitConfigEmail('agarciaf@outlook.es')
+    }
+  }
+  
+  parameters {
+    stringParam('nombre', defaultValue = 'operador', description = 'Parametro de cadena para el Job Booleano')
+    choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    booleanParam('agente', false)
+  }
+  
+  triggers {
+    cron('H/7 * * * *')
+  }
+  steps {
+    shell("bash job-scritp.sh")
+  }
+  
+  publishers {
+    mailer('agarciaf@outlook.es', true, true)
+      
+  }
+}
